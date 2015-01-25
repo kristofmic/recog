78d8557423561acda04412a71a4c32a80bf5d632
Chris Hourihan <chourihan@gmail.com>
2015-01-25 14:00:05 -0500

    updating redis config

--- tasks/grunt/env.js
+++ tasks/grunt/env.js
@@ -1,9 +1,11 @@
 module.exports = {
   dev: {
-    NODE_ENV: 'localhost'
+    NODE_ENV: 'localhost',
+    REDIS_HOST: '127.0.0.1'
   },
   test: {
-    NODE_ENV: 'test'
+    NODE_ENV: 'test',
+    REDIS_HOST: '127.0.0.1'
   },
   production: {
     NODE_ENV: 'production'
